271d1013541de312c2a6f80668a0d5ef49b840b0
= <=>
2018-04-02 15:24:23 +0530

    recaptcha  validaition

--- index.php
+++ index.php
@@ -2,3 +2,19 @@
 phpinfo();
 
 ?>
+
+
+
+
+//------------------------------- Recaptcha Validate ----------------------------------------------//
+
+	$rcaptchaKey =  variable_get('recaptcha_site_key');
+	$rcaptchaSecret =   variable_get('recaptcha_secret_key');
+	$recaptcha = new \ReCaptcha\ReCaptcha($rcaptchaSecret);
+	$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+	if (!$resp->isSuccess()):
+		
+	endif;
+
+//----------------------------------- Recaptcha Validate ------------------------------------------//
+
